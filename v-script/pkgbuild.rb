0b848a77b10d2b2fd0a9a1f4f2f7e862044e5d67
Kilobyte <stiepen22@gmx.de>
2014-06-26 19:58:54 +0200

    Fixed a bug with package stuff

--- v-script/pkgbuild.rb
+++ v-script/pkgbuild.rb
@@ -3,4 +3,8 @@ name 'V-Script'
 
 description 'A Lisp Like scripting language for OC'
 
+install 'v-script.lua' => '/lib'
+install Dir['v-script/lib/v-script/*.lua'] => '/lib/v-script'
+install Dir['v-script/lib/v-script/values/*.lua'] => '/lib/v-script/values'
+
 depend :'oop-system'
